6f1c8bfcdae851dceda9a02034836502ccb1eaf1
ZiadAhmed07 <ziad07138@gmail.com>
2024-06-27 15:55:41 +0300

    y

--- src/app/(dashboard)/dashboard/_components/Stateus.js
+++ src/app/(dashboard)/dashboard/_components/Stateus.js
@@ -1,104 +1,97 @@
-"use client"
+'use client'
 
 import { Domin } from '@/api/data'
 import axios from 'axios'
 import { getCookie } from 'cookies-next'
-import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
-export default function Stateus() {
-
-    const [data, setData] = useState(null)
-    const [state , setState] = useState({
-        Users : 0,
-        Comments : 0,
-        Contacts:0,
-        Jobs : 0,
-        Orders:0,
-        Teams : 0,
-        Projects : 0,
-        
-    })
-
-    let userData = false
-    if (getCookie('userData')) {
-        const user = JSON.parse(getCookie('userData'))
-        userData = user
-    } 
-
-    useEffect(()=>{
-        if(data){
-            console.log(data)
-            setState({
-                Users:data.Users_count,
-                Comments:data.Comments_count,
-                Contacts:data.Contacts_count,
-                Jobs:data.Jobs_count,
-                Orders:data.Orders_count,
-                Teams:data.Teams_count,
-                Projects:data.Projects_count
-            })
-        }
-    },[data])
+export default function Status() {
+    const [data, setData] = useState(null);
+    const [state, setState] = useState({
+        Users: 0,
+        Comments: 0,
+        Contacts: 0,
+        Jobs: 0,
+        Orders: 0,
+        Teams: 0,
+        Projects: 0,
+    });
 
     useEffect(() => {
-        axios({
-            method: 'get',
-            url: `${Domin}/api/admin/showAll/statistics`,
-            headers: {
-                'Authorization': `Bearer ${userData.token}`
+        const fetchData = async () => {
+            try {
+                const userData = JSON.parse(getCookie('userData'));
+                const response = await axios.get(`${Domin}/api/admin/showAll/statistics`, {
+                    headers: {
+                        'Authorization': `Bearer ${userData.token}`
+                    }
+                });
+                setData(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
             }
-        }).then((res) => {
-            setData(res.data)
-        })
-    }, [])
+        };
 
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        if (data) {
+            setState({
+                Users: data.Users_count,
+                Comments: data.Comments_count,
+                Contacts: data.Contacts_count,
+                Jobs: data.Jobs_count,
+                Orders: data.Orders_count,
+                Teams: data.Teams_count,
+                Projects: data.Projects_count
+            });
+        }
+    }, [data]);
+
+    function checkData() {
+        if (data) {
+            console.log(data)
+            return (
+                <div className='w-full h-full flex items-end justify-around pr-[50px] pb-[50px]'>
+                    <div className={`w-[50px] transition-all h-[${state.Users * 10}px] bg-yellow-500`}></div>
+                    <div className={`w-[50px] transition-all h-[${state.Comments * 10}px] bg-orange-500`}></div>
+                    <div className={`w-[50px] transition-all h-[${state.Contacts * 10}px] bg-pink-500`}></div>
+                    <div className={`w-[50px] transition-all h-[${state.Jobs * 10}px] bg-green-600`}></div>
+                    <div className={`w-[50px] transition-all h-[${state.Orders * 10}px] bg-blue-500`}></div>
+                    <div className={`w-[50px] transition-all h-[${state.Teams * 10}px] bg-red-500`}></div>
+                    <div className={`w-[50px] transition-all h-[${state.Projects * 10}px] bg-purple-500`}></div>
+                </div>
+            )
+        }
+    }
 
     return (
         <div className='w-full overflow-auto'>
-            <div className='min-w-[750px] relative '>
+            <div className='min-w-[750px] relative'>
                 <div className='w-[50px] h-[400px] border-l-4 border-solid border-gray-700 flex flex-col-reverse items-center justify-between'>
-                    <p>0</p>
-                    <p>5</p>
-                    <p>10</p>
-                    <p>15</p>
-                    <p>20</p>
-                    <p>25</p>
-                    <p>30</p>
-                    <p>35</p>
-                    <p>40</p>
+                    {[0, 5, 10, 15, 20, 25, 30, 35, 40].map((value) => (
+                        <p key={value}>{value}</p>
+                    ))}
                 </div>
                 <div className='w-full h-[50px] border-t-4 border-solid border-gray-700 flex items-center justify-around gap-1 pr-[50px]'>
-                    <p className=' w-[50px] text-center font-bold'>المستخدمين</p>
-                    <p className=' w-[50px] text-center font-bold'>التعليقات</p>
-                    <p className=' w-[50px] text-center font-bold'>الرسائل</p>
-                    <p className=' w-[50px] text-center font-bold'>الوظائف</p>
-                    <p className=' w-[50px] text-center font-bold'>الطلبات</p>
-                    <p className=' w-[50px] text-center font-bold'>الفريق</p>
-                    <p className=' w-[50px] text-center font-bold'>المشاريع</p>
+                    <p className='w-[50px] text-center font-bold'>المستخدمين</p>
+                    <p className='w-[50px] text-center font-bold'>التعليقات</p>
+                    <p className='w-[50px] text-center font-bold'>الرسائل</p>
+                    <p className='w-[50px] text-center font-bold'>الوظائف</p>
+                    <p className='w-[50px] text-center font-bold'>الطلبات</p>
+                    <p className='w-[50px] text-center font-bold'>الفريق</p>
+                    <p className='w-[50px] text-center font-bold'>المشاريع</p>
                 </div>
                 <div className="absolute top-0 w-full h-full overflow-hidden">
                     <div className='h-full w-full absolute right-[50px]'>
-                        <div className='border-t-[1px] border-gray-800 h-[50px]'></div>
-                        <div className='border-t-[1px] border-gray-800 h-[50px]'></div>
-                        <div className='border-t-[1px] border-gray-800 h-[50px]'></div>
-                        <div className='border-t-[1px] border-gray-800 h-[50px]'></div>
-                        <div className='border-t-[1px] border-gray-800 h-[50px]'></div>
-                        <div className='border-t-[1px] border-gray-800 h-[50px]'></div>
-                        <div className='border-t-[1px] border-gray-800 h-[50px]'></div>
-                        <div className='border-t-[1px] border-gray-800 h-[50px]'></div>
+                        {[...Array(8)].map((_, index) => (
+                            <div key={index} className='border-t-[1px] border-gray-800 h-[50px]'></div>
+                        ))}
                     </div>
-                    <div className=' w-full h-full flex items-end justify-around pr-[50px] pb-[50px]'>
-                        <div className={`w-[50px] transition-all h-[${state.Users}0px] bg-yellow-500`}></div>
-                        <div className={`w-[50px] transition-all h-[${state.Comments}0px] bg-orange-500`}></div>
-                        <div className={`w-[50px] transition-all h-[${state.Contacts}0px] bg-pink-500`}></div>
-                        <div className={`w-[50px] transition-all h-[${state.Jobs}0px] bg-green-600`}></div>
-                        <div className={`w-[50px] transition-all h-[${state.Orders}0px] bg-blue-500`}></div>
-                        <div className={`w-[50px] transition-all h-[${state.Teams}0px] bg-red-500`}></div>
-                        <div className={`w-[50px] transition-all h-[${state.Projects}0px] bg-purple-500`}></div>
-                    </div>
+                    {checkData()}
                 </div>
             </div>
         </div>
-    )
+    );
 }
--- src/app/[locale]/(Home)/page.js
+++ src/app/[locale]/(Home)/page.js
@@ -1,51 +1,50 @@
 import Image from "next/image";
 import Link from "next/link";
-import imgHeroSection from "/public/images/hero.png"
-import bgHero from "/public/images/bg-pattern.webp"
-import "./HomeStyle.css"
+import bgHero from "/public/images/bg-pattern.webp";
+import "./HomeStyle.css";
 import Gradients from "@/components/gradients/gradients";
 import About from "@/components/AboutSection/About";
 import Feature from "@/components/Feature/Feature";
 import Stats from "@/components/Stats/Stats";
 import Pricing from "@/components/Pricing/Pricing";
 import Testimonials from "@/components/Testimonials/Testimonials";
-import {useTranslations} from 'next-intl';
+import { useTranslations } from 'next-intl';
 
-
-export default function Home({ params}) {
-
+export default function Home({ params }) {
   const t = useTranslations('Index');
 
   return (
-    <main id={"homePage"}>
-      <Gradients/>
-      <section id="heroSection" className="animation-BottonToTop"> 
-      <div id={"DivMainTextHeroSection"}>
-        <Image src={bgHero} alt="..." id={"bgHero"} width={1000} priority height={1000} />
-        <div id={"textHeroSection"}>
-          <span>{t("home.heroSection.1")}</span>
-          <h2 className=" w-full md:w-[700px]">{t("home.heroSection.2")}</h2>
-          <p>{t("home.heroSection.3")}</p>
-          <button className="button">
-            <Link href={`/${params.locale}/order`} className=" w-full h-full pt-2">{t("home.heroSection.4")}</Link>
-          </button>
+    <main id="homePage">
+      <Gradients />
+      <section id="heroSection" className="animation-BottonToTop">
+        <div id="DivMainTextHeroSection">
+          <Image src={bgHero} alt="..." id="bgHero" width={1000} priority height={1000} />
+          <div id="textHeroSection">
+            <span>{t("home.heroSection.1")}</span>
+            <h2 className="w-full md:w-[700px]">{t("home.heroSection.2")}</h2>
+            <p>{t("home.heroSection.3")}</p>
+            <button className="button">
+              <Link href={`/${params.locale}/order`} className="w-full h-full pt-2">
+                {t("home.heroSection.4")}
+              </Link>
+            </button>
+          </div>
         </div>
-      </div> 
-        <div id="imgHeroSection">
-          <Image src={imgHeroSection} alt="imgHeroSection"/>
-          <span></span>
+        <div id="imgHeroSection" className="flex items-center justify-center flex-col">
+          <video controls autoPlay muted loop className="w-full h-full border-gray-200 border-solid border-[10px] sm:border-[13px] md:border-[20px]  rounded-2xl">
+            <source src="/video1.mp4" type="video/mp4" />
+          </video>
+          <div className="w-[130px] rounded-3xl sm:w-[180px] lg:w-[200px] border-[50px] sm:border-[70px] border-red-500/0 border-b-gray-200 -translate-y-[50px] sm:-translate-y-[70px]">
+          </div>
         </div>
       </section>
-      <About/>
-
-      <Feature/>
+      <About />
+      <Feature />
       <div id="Testimonials">
-        <Testimonials/>
-      </div>    
-     
-      <Stats/>
-      <Pricing/>
-      
+        <Testimonials />
+      </div>
+      <Stats />
+      <Pricing />
     </main>
   );
 }
